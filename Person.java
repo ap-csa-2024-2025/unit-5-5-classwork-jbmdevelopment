f0dfa88bdce10debbda0721be086ce6eadb49893
jbmdevelopment <jbarreramartinez100@student.hbuhsd.edu>
2024-12-09 19:18:53 +0000

    a

--- Person.java
+++ Person.java
@@ -1,4 +1,19 @@
 public class Person
 {
-
+    private String first;
+    private String last;
+    private int age;
+    private String SSN;
+    public setFirst(String name) {
+        first = name;
+    }
+    public setLast(String name) {
+        last = name;
+    }
+    public setAge(int x) {
+        age = x;
+    }
+    public setSSN(String s) {
+        SSn = s;
+    }
 }
