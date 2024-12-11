9c580c1350ba0565c339c98d98c33a77fb7809e1
jbmdevelopment <jbarreramartinez100@student.hbuhsd.edu>
2024-12-11 19:02:33 +0000

    aa

--- Oven.java
+++ Oven.java
@@ -1,4 +1,24 @@
 public class Oven
 {
+    private int maxTemp;
+    private int currentTemp;
+    public int getMaxTemp() {
+        return maxTemp;
+    }
+    public int getCurrentTemp() {
+        return currentTemp;
+    }
+    public void turnOff() {
 
-}
+    }
+    public boolean isOn() {
+
+    }
+    public void preheat(int temp) {
+
+    }
+    public String toString() {
+        String formatted = "";
+        return formatted;
+    }
+} 
