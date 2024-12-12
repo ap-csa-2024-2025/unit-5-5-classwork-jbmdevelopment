e2192530b1db2b8e0393db5d73104e47ad0c6bae
jbmdevelopment <jbarreramartinez100@student.hbuhsd.edu>
2024-12-12 21:47:49 +0000

    aa

--- Oven.java
+++ Oven.java
@@ -11,7 +11,7 @@ public class Oven
         return currentTemp;
     }
     public String updateStatus() {
-       return "New ven with a maximum temperature of " + maxTemp + " and a starting temperature of " + currentTemp + " degrees.";
+       return "New oven with a maximum temperature of " + maxTemp + " and a starting temperature of " + currentTemp + " degrees.";
 
     }
     public void turnOff() {
